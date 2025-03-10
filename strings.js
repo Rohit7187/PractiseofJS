4367b269c177832c49874f310eaaba1cf5551bc6
Rohit7187 <rohitnakhrekar28@gmail.com>
2025-03-11 01:27:11 +0530

    practise

---
+++ strings.js
@@ -0,0 +1,12 @@
+
+function addThree(a , b , c){
+    return a + b + c;
+}
+let c = addThree(1, 2, 65);
+console.log(c);
+
+function removeLastDigit(a , b , c,){
+    return a + b;
+}
+let d = removeLastDigit(2, 65 , 6)
+console.log(d)
