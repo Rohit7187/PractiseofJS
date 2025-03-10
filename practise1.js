6716cc3d249dae9a360f7ea923f28353e171e8bd
Rohit7187 <rohitnakhrekar28@gmail.com>
2025-03-11 02:29:15 +0530

    practise1

---
+++ practise1.js
@@ -0,0 +1,16 @@
+let a = "Rohit";
+let n = 2;
+
+console.log(a+n);
+
+console.log(typeof a);
+
+// create a distionory
+
+const distionory = {
+    alok : "a creatu",
+    jhol : "chomu",
+    goti : "game"
+}
+
+console.log(distionory.goti);
---
+++ variable.js
@@ -0,0 +1,29 @@
+//console.log("My practise");
+//var a = 45;
+//console.log(a);
+
+//var let and const are 3 things.
+
+//premitive data type- NN SS BB U(Null Number String Symbol Bolean Bigint Undefined)
+let a = null;
+let b = 12;
+let c = Symbol("a");
+let d = "hhh";
+let e = true;
+let f = BigInt("56")+ BigInt("4");
+let g = undefined;
+
+console.log(a,b,c,d,e,f,g);
+console.log(d);
+
+//Non-premitive
+//-object
+
+const item = {
+    "a" : true,
+    "n" : false,
+    "love" : 547,
+}
+
+console.log(item["love"]);
+
