3fc8626bee2a5cf3d6261c847570a657ac9dc7dc
Rohit7187 <rohitnakhrekar28@gmail.com>
2025-03-11 03:42:46 +0530

    operatos

---
+++ practise3.js
@@ -0,0 +1,13 @@
+// let a = prompt("WHats your age?");
+// a = Number.parseInt(a); //type casting - number to string
+// console.log(a);
+
+// if(a > 0){
+//     alert("This is valid");
+// }
+// else {
+//     alert("not valid");
+// } 
+
+let b = 2;
+console.log("you can", (b > 18 ? "drive" : "not drive"));
