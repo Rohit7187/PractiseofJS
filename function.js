c7adb05828aee5cdcd0d54a147a65fe751a4747c
Rohit7187 <rohitnakhrekar28@gmail.com>
2025-03-14 03:43:09 +0530

    update

--- function.js
+++ function.js
@@ -1,2 +1,38 @@
 // Functions
 
+//  const hello = () => {
+//     console.log("Hello wordl");
+//     return "hi";
+// }
+
+
+// hello();
+
+// function rohit(p,q){
+//     return (p+q);
+// }
+
+// let a = 1;
+// let b = 2;
+
+// console.log("sum",rohit(a,b));
+
+// enter a correct number
+
+// let cn = 45;
+// let i;
+
+// while(i != cn){
+//     i = prompt("enter a correct number");
+//     console.log("try again");
+// }
+
+// console.log("correct number");
+
+//arrow function
+
+const arrow = (a,b,c,d) => {
+    return((a+b+c+d)/2);
+}
+
+console.log(arrow(4,5,6,7));
