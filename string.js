5b7237e59b7bec10248510cfdfbab10f8cc5d81c
Rohit7187 <rohitnakhrekar28@gmail.com>
2025-03-14 03:59:52 +0530

    updated file

---
+++ string.js
@@ -0,0 +1,16 @@
+//string interpulation-interview question
+// ``- backslash denoted ${}
+
+// let boy1 = "akshay";
+// let boy2 = "Raj";
+
+// let sent = `${boy2} is friend of ${boy1}`
+
+// console.log(sent);
+
+
+
+//backslash- escape sequesnce
+
+let fruit = `apple\ne`;
+console.log(fruit)
