59c75c4ce27891eaae9c5f384469df8c9a751e94
Rohit7187 <rohitnakhrekar28@gmail.com>
2025-03-14 02:50:55 +0530

    function.js

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
+//for loop
+
 // for(let i = 0; i <= 20; i++){
 //     console.log(i);
 // }
@@ -10,11 +12,31 @@
 // }
 
 
-let N = 5; // You can change this to any non-negative integer
-let factorial = 1;
+// let N = 5; // You can change this to any non-negative integer
+// let factorial = 1;
 
-for(let i = 1; i <= N; i++) {
-    factorial *= i;
-}
+// for(let i = 1; i <= N; i++) {
+//     factorial *= i;
+// }
 
-console.log(`The factorial of ${N} is ${factorial}`);
+// console.log(`The factorial of ${N} is ${factorial}`);
+
+// let sum = 0;
+// for(let i = 1; i <= 5; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+
+
+
+
+// while loop
+// let n = prompt("enter the number");
+// n = Number.parseInt(n);
+// let i = 110;
+// while(i < n){
+//     console.log(i);
+//     i++;
+// }
