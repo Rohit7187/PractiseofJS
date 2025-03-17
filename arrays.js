cde1e71b6db28ca9a97ff1c8b1095f309d5aa01a
Rohit7187 <rohitnakhrekar28@gmail.com>
2025-03-18 01:12:19 +0530

    card project

--- arrays.js
+++ arrays.js
@@ -18,3 +18,41 @@ for(let i = 0; i < arr.length; i++){
 // for(let hero of heroes){
 //     console.log(hero);
 // }
+
+// let marks = [85,97,44,37,76,60];
+// let sum = 0;
+
+// for(let i=0; i< marks.length; i++){
+//     sum = sum + marks[i];
+// }
+// console.log(sum);
+
+// let avg = sum/marks.length;
+// console.log(avg);
+
+// for(let val of marks){
+//     sum = sum + val;
+// }
+
+// console.log(sum/marks.length);
+
+// let val = [250,645,300,900,50];
+// let offer = 0;
+// realVal = 0;
+// for(let prize of val){
+
+//     offer = val/10;
+//     realVal = val - offer;
+//     console.log(realVal);
+// }
+
+//let str = "Apnacolleage";
+
+function countVowles(str){
+    for(const char of str);
+    console.log(char);
+}
+
+
+
+
